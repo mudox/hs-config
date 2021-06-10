af531eb198aab61e87a856bb3a5373ff2ecd2b39
mudox <imudox@gmail.com>
2021-06-11 00:59:35 +0800

    init.lua: reenable window animation

--- init.lua
+++ init.lua
@@ -1,10 +1,12 @@
 -- vim: fdm=marker
-
 -- Reload Configuration
 -- Pin it to fist line
 require 'lib.reload'
 
 local bind = require 'lib.bind'
+
+-- Disable window animation
+-- hs.window.animationDuration = 0
 
 -- Inspect value in console
 ---@diagnostic disable-next-line: lowercase-global
@@ -15,12 +17,6 @@ bind.altShift('x', hs.toggleConsole)
 
 -- Grid mode
 require('lib.grid')
-bind.altShift('3', function()
-   hs.grid.toggleShow(nil, true) 
-end)
-
--- Disable window animation
-hs.window.animationDuration = 0
 
 -- Alert default style
 hs.alert.defaultStyle.radius = 4
@@ -29,7 +25,7 @@ hs.alert.defaultStyle.strokeWidth = 0.5
 -- Root chooser
 ---@diagnostic disable-next-line: lowercase-global
 rootChooser = require('rootchooser')
-bind.alt('r', function ()
+bind.alt('r', function()
   rootChooser:show()
 end)
 
