12b2c697eee07bfbaf96f508d7f4019e172e9c39
mudox <imudox@gmail.com>
2021-06-11 00:56:07 +0800

    airpods: fix & improve code

--- lib/airpods.lua
+++ lib/airpods.lua
@@ -1,44 +1,68 @@
 local cmd = os.getenv('HOME') .. '/Git/hs-config/script/airpods.sh'
+local name = 'NBN AirPods'
 
+--- Toggle connection status
+-- @return 'connect' or 'disconnect'
 local function toggle()
   local output = hs.execute('sh ' .. cmd .. ' toggle')
-  hs.alert(output:gsub('%s*$', '', 1), 3)
+  return output:gsub('%s*$', '', 1), 3
 end
 
+--- Check airpods connection status
+-- @return 'connected' or 'disconnected'
 local function status()
   local output = hs.execute('sh ' .. cmd .. ' status')
-  hs.alert(output:gsub('%s*$', '', 1), 3)
+  return output:gsub('%s*$', '', 1), 3
 end
 
 -- Chooser item
-local toggleAirPods = {
-  text = 'Toggle AirPods',
-  subText = 'Connect / Disconnect NBN AirPods',
-  image = hs.image.imageFromName('NSBluetoothTemplate'),
-
-  action = function()
-    require('lib.airpods').toggle()
-  end
-}
-
--- Chooser item
-local showAirPodsStatus = {
-  text = 'Show AirPods Status',
-  subText = 'Show if AirPods is connected or not',
-  image = hs.image.imageFromName('NSBluetoothTemplate'),
-
-  action = function()
-    require('lib.airpods').status()
-  end
-}
 
 -- Module
 
 local m = {
   toggle = toggle,
-  status = status, 
-  toggleAirPods = toggleAirPods,
-  showAirPodsStatus = showAirPodsStatus,
+  status = status,
+
+  chooserItems = {
+
+    toggle = {
+      text = 'Toggle AirPods',
+      subText = 'Connect / Disconnect NBN AirPods',
+      image = hs.image.imageFromName('NSBluetoothTemplate'),
+
+      action = function()
+        local s = toggle()
+        local msg = ''
+
+        if s == 'connect' then
+          msg = ('Connecting %s'):format(name)
+        else
+          msg = ('Disconnecting %s'):format(name)
+        end
+
+        hs.alert(msg)
+      end
+    },
+
+    check = {
+      text = 'Show AirPods Status',
+      subText = 'Show if AirPods is connected or not',
+      image = hs.image.imageFromName('NSBluetoothTemplate'),
+
+      action = function()
+        local s = status()
+        local msg
+
+        if s == 'connected' then
+          msg = ('%s Conncted'):format(name)
+        else
+          msg = ('%s Disconncted'):format(name)
+        end
+
+        hs.alert(msg)
+      end
+    }
+  }
 }
 
 return m
--- script/airpods.sh
+++ script/airpods.sh
@@ -1,6 +1,6 @@
 #!/user/bin/env bash
 
-# Manages bluetooth connectivity of AirPods 
+# Manages bluetooth connectivity of AirPods
 
 if [[ "$#" -ne 1 || ("$1" != "toggle" && "$1" != "status") ]]; then
   echo "Usage: airpods [toggle|status]"
@@ -13,26 +13,34 @@ ID='38-ec-0d-58-93-7f' # MAC address
 
 CMD='/opt/homebrew/bin/blueutil'
 
-if ! "${CMD}" --info "${ID}" &>/dev/null; then 
+is_connected() {
+  if [[ $(${CMD} --is-connected ${ID}) -eq 1 ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+if ! "${CMD}" --info "${ID}" &>/dev/null; then
   echo "AirPods mac address ${ID} is invalid"
 fi
 
 case $1 in
-  toggle)
-    if $CMD --is-connected "$ID" &>/dev/null; then
-      echo "Connecting ${NAME}..."
-      $CMD --connect "$ID" >/dev/null 2>&1 &
-    else
-      echo "Disconnecting ${NAME}..."
-      $CMD --disconnect "$ID" >/dev/null 2>&1 &
-    fi
-    ;;
+toggle)
+  if ! is_connected; then
+    echo 'connect'
+    $CMD --connect "$ID" &>/dev/null 2>&1 &
+  else
+    echo 'disconnect'
+    $CMD --disconnect "$ID" &>/dev/null 2>&1 &
+  fi
+  ;;
 
-  status)
-    if $CMD --is-connected "$ID" &>/dev/null; then
-      echo "${NAME} Disconnected"
-    else
-      echo "${NAME} Connected"
-    fi
-    ;;
+status)
+  if is_connected &>/dev/null; then
+    echo 'connected'
+  else
+    echo 'disconnected'
+  fi
+  ;;
 esac
