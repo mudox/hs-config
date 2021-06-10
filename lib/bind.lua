6715a83df975719bb627e3348c4860c23912492a
mudox <imudox@gmail.com>
2021-06-11 00:56:45 +0800

    lib.bind: add `ctrlAlt`

--- lib/bind.lua
+++ lib/bind.lua
@@ -1,11 +1,21 @@
-local alt = {"alt"}
-local altShift = {"alt", "shift"}
-local cmdAlt = {"cmd", "alt"}
-local ctrlCmd = {"ctrl", "cmd"}
-
 return {
-  alt = function(key, to) hs.hotkey.bind(alt, key, to) end,
-  altShift = function(key, to) hs.hotkey.bind(altShift, key, to) end,
-  cmdAlt  = function(key, to) hs.hotkey.bind(cmdAlt, key, to) end,
-  ctrlCmd  = function(key, to) hs.hotkey.bind(ctrlCmd, key, to) end,
+  alt = function(key, to, ...)
+    hs.hotkey.bind({'alt'}, key, to, ...)
+  end,
+
+  altShift = function(key, to, ...)
+    hs.hotkey.bind({'alt', 'shift'}, key, to, ...)
+  end,
+
+  cmdAlt = function(key, to, ...)
+    hs.hotkey.bind({'cmd', 'alt'}, key, to, ...)
+  end,
+
+  ctrlCmd = function(key, to, ...)
+    hs.hotkey.bind({'ctrl', 'cmd'}, key, to, ...)
+  end,
+
+  ctrlAlt = function(key, to, ...)
+    hs.hotkey.bind({'ctrl', 'alt'}, key, to, ...)
+  end
 }
