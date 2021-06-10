97a89e7e40c44ac6757b0fcc94b9e15caadabe45
mudox <imudox@gmail.com>
2021-06-11 00:57:48 +0800

    lib.macos: add `lockScreen`

--- lib/macos.lua
+++ lib/macos.lua
@@ -1,32 +1,27 @@
 -- system often auto-wake up from this kind of sleep
 -- use `displaySleep` instead
 local function sleep()
-    hs.execute([[pmset sleepnow]])
+  hs.caffeinate.systemSleep()
+  -- hs.execute([[pmset sleepnow]]) 
 end
 
 local function dispalySleep()
-    hs.execute([[pmset displaysleepnow]])
+  hs.execute([[pmset displaysleepnow]])
 end
 
-local _sleep = {
-    text = 'Sleep',
-    subText = 'Pub system into sleep, use `Display Sleep` instead',
-    
-    action = sleep,
-}
-
-local _displaySleep = {
-    text = 'Display Sleep',
-    subText = 'Pub display into sleep',
-    
-    action = dispalySleep,
-}
+local function lockScreen()
+  hs.caffeinate.lockScreen()
+end
 
 -- Module
 return {
-    sleep = sleep,
-    dispalySleep = dispalySleep,
-    
-    itemSleep = _sleep,
-    itemDisplaySleep = _displaySleep,
+  sleep = sleep,
+  dispalySleep = dispalySleep,
+  lockScreen = lockScreen,
+
+  chooserItems = {
+    sleep = {text = 'Sleep', action = sleep},
+    displayScreen = {text = 'Display Sleep', action = dispalySleep},
+    lockScreen = {text = 'Lock Screen', action = lockScreen}
+  }
 }
