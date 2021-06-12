684b838f027d07ca9386a63f5450e4d9f9fafc96
mudox <imudox@gmail.com>
2021-06-12 19:07:58 +0800

    lib.bind: publicize mod keys

--- lib/bind.lua
+++ lib/bind.lua
@@ -1,21 +1,35 @@
+local alt = {'alt'}
+local altShift = {'alt', 'shift'}
+local altCmd = {'alt', 'cmd'}
+local ctrlCmd = {'ctrl', 'cmd'}
+local ctrlAlt = {'ctrl', 'alt'}
+
 return {
+  mods = {
+    alt = alt,
+    altShift = altShift,
+    altCmd = altCmd,
+    ctrlCmd = ctrlCmd,
+    ctrlAlt = ctrlAlt,
+  },
+
   alt = function(key, to, ...)
-    hs.hotkey.bind({'alt'}, key, to, ...)
+    hs.hotkey.bind(alt, key, to, ...)
   end,
 
   altShift = function(key, to, ...)
-    hs.hotkey.bind({'alt', 'shift'}, key, to, ...)
+    hs.hotkey.bind(altShift, key, to, ...)
   end,
 
   cmdAlt = function(key, to, ...)
-    hs.hotkey.bind({'cmd', 'alt'}, key, to, ...)
+    hs.hotkey.bind(altCmd, key, to, ...)
   end,
 
   ctrlCmd = function(key, to, ...)
-    hs.hotkey.bind({'ctrl', 'cmd'}, key, to, ...)
+    hs.hotkey.bind(ctrlCmd, key, to, ...)
   end,
 
   ctrlAlt = function(key, to, ...)
-    hs.hotkey.bind({'ctrl', 'alt'}, key, to, ...)
-  end
+    hs.hotkey.bind(ctrlAlt, key, to, ...)
+  end,
 }
