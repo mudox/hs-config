c13ca08c3c1e55aa3a6e3dbaf9c7d64d2164aa53
mudox <imudox@gmail.com>
2021-06-21 05:58:13 +0800

    filter: re-fullscreen `Kitty` & `iTerm`

--- lib/filter.lua
+++ lib/filter.lua
@@ -1,5 +1,5 @@
 local log = hs.logger.new('lib.filter')
-log.setLogLevel('debug')
+log.setLogLevel('info')
 
 local wf = hs.window.filter
 
@@ -28,9 +28,39 @@ mid.w = gap
 wf.new()
   :setDefaultFilter({allowRegions = {top, bottom, left, right, mid}})
   :subscribe(wf.windowUnfocused,function(win, name, event)
-    if name ~= hs.application.frontmostApplication():name() then
-      log.df('hide %s[%s]', name, win:title())
-      win:application():hide()
+    log.df(('windowUnFocused: %s'):format(name))
+
+    local names = {'iTerm2', 'kitty', 'Firefox'}
+    if fx.contains(names, name) then
+      if name ~= hs.application.frontmostApplication():name() then
+        log.df('hide %s', name)
+        win:application():hide()
+      end
+    end
+  end):pause()
+-- LuaFormatter on
+
+local function needReFullscreen(win)
+  local f = win:frame()
+  local sf = hs.screen.mainScreen():frame();
+  local i = sf:intersect(f)
+
+  if i.area / sf.area > 0.6 then
+    log.f(('Re-fullscreen %s'):format(name))
+    require('lib.layout').fullscreen(win)
+  end
+end
+
+-- LuaFormatter off
+wf.new({'iTerm2', 'kitty'})
+  :subscribe(wf.windowFocused,function(win, name, event)
+    log.df(('windowFocused: %s'):format(name))
+
+    if needReFullscreen(win) then
+      log.f(('Re-fullscreen %s'):format(name))
+      local layout = require("lib.layout")
+      layout.fullscreen(win)
+      layout.onlyShow(win:application():bundleID())
     end
   end)
 -- LuaFormatter on
