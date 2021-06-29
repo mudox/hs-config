11489c26202e9e31368a6d096fccb50bb6ace685
mudox <imudox@gmail.com>
2021-06-30 06:20:25 +0800

    improve `snaptocell`

--- lib/filter/snaptocell.lua
+++ lib/filter/snaptocell.lua
@@ -14,9 +14,16 @@ local function rectApproximate(left, right, tolerance)
            math.abs(left.y2 - right.y2) < tolerance
 end
 
+local sheetApps = {'Session'}
+local fullscreenApps = {'Tower', 'Dash', 'Firefox', 'Xcode'}
+
 local f = wf.new()
 f:setDefaultFilter({allowRoles = {'AXStandardWindow'}})
-f:setAppFilter('Alfred', false)
+
+f:rejectApp('Alfred')
+f:rejectApp('SetappAgent')
+f:rejectApp('Setapp')
+
 f:subscribe(wf.windowOnScreen, function(win, name, event)
   log.df(('Event windowOnScreen: %s'):format(name))
 
@@ -27,14 +34,31 @@ f:subscribe(wf.windowOnScreen, function(win, name, event)
     layout.fullscreen(win)
   elseif rectApproximate(f, cell.grid12:cell(1, 1)) then
     log.f(('Dectect layout [left half] -> %s'):format(name))
+    -- currently noop
   elseif rectApproximate(f, cell.grid12:cell(1, 2)) then
     log.f(('Detect layout [right half] -> %s'):format(name))
+    -- currently noop
   elseif rectApproximate(f, cell.sheet()) then
     log.f(('Detect layout [sheet] -> %s'):format(name))
+    -- currently noop
   elseif rectApproximate(f, cell.form()) then
     log.f(('Detect layout [form] -> %s'):format(name))
+    -- currently noop
   else
-    log.f(('Found unknown frame layout, apply layout [form] -> %s'):format(name))
-    layout.form(win)
+    -- do nothing if the application open multiple windows
+    if #win:application():allWindows() > 1 then
+      return
+    end
+
+    if fx.contains(fullscreenApps, name) then
+      log.f(('Apply layout [fullscreen] -> %s'):format(name))
+      layout.fullscreen(win)
+    elseif fx.contains(sheetApps, name) then
+      log.f(('Apply layout [sheet] -> %s'):format(name))
+      layout.sheet(win)
+    else
+      log.f(('Apply layout [form] -> %s'):format(name))
+      layout.form(win)
+    end
   end
 end)
