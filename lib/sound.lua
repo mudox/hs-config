089f269f1f4b564dc1f20b392d6cc3babfc265b3
mudox <imudox@gmail.com>
2021-08-04 21:56:02 +0800

    improve sound.lua

--- lib/sound.lua
+++ lib/sound.lua
@@ -3,12 +3,8 @@ local au = hs.audiodevice
 local step = 5
 
 local function outDevice()
-  local d = au.current().device
-  if not d then
-    hs.alert('No current output device')
-  else
-    return d
-  end
+  local d = hs.audiodevice.current()
+  return d.volume and d.device or nil
 end
 
 local alertID
@@ -17,10 +13,17 @@ local function round(n)
   return n % 1 >= 5 and math.ceil(n) or math.floor(n)
 end
 
-local tink = hs.sound.getByName('Tink')
+local function tink()
+  return hs.sound.getByName('Tink')
+end
 
 local function volumeUp()
   local dev = outDevice()
+  if not dev then
+    hs.alert('Device does not supporting changing volume')
+    return
+  end
+
   local vol = dev:volume()
 
   if vol < 100 then
@@ -29,7 +32,7 @@ local function volumeUp()
     vol = math.min(100, vol)
 
     dev:setVolume(vol)
-    tink:stop():play()
+    tink():stop():play()
   end
 
   hs.alert.closeSpecific(alertID)
@@ -38,6 +41,11 @@ end
 
 local function volumeDown()
   local dev = outDevice()
+  if not dev then
+    hs.alert('Device does not supporting changing volume')
+    return
+  end
+
   local vol = dev:volume()
 
   if vol > 0 then
@@ -46,7 +54,7 @@ local function volumeDown()
     vol = math.max(0, vol)
 
     dev:setVolume(vol)
-    tink:stop():play()
+    tink():stop():play()
   end
 
   hs.alert.closeSpecific(alertID)
