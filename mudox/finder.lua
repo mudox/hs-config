cbfbe71987dc884fc29e11ab8a4a698cfdb50cd0
Mudox <imudox@gmail.com>
2022-09-05 03:34:46 +0800

    add chooser item to open apple folder

--- mudox/finder.lua
+++ mudox/finder.lua
@@ -1,29 +1,29 @@
 local ids = require("mudox.bundle").ids
 local finderIcon = hs.image.imageFromAppBundle(ids.finder)
 
-local xcodeLibrary = {
-  text = "Open Xcode Library folder",
-  subText = "~/Library/Developer/Xcode",
-  image = finderIcon,
-
-  action = function()
-    os.execute("open ~/Library/Developer/Xcode")
-  end,
-}
-
-local xcodeDerivedData = {
-  text = "Open Xcode DerivedData folder",
-  subText = "~/Library/Developer/Xcode/DerivedData",
-  image = finderIcon,
-
-  action = function()
-    os.execute("open ~/Library/Developer/Xcode/DerivedData")
-  end,
+local items = {
+  {
+    text = "Finder: Xcode Library folder",
+    path = "~/Library/Developer/Xcode",
+  },
+  {
+    text = "Finder: Xcode DerivedData folder",
+    path = "~/Library/Developer/Xcode/DerivedData",
+  },
+  {
+    text = "Finder: Develop/Apple folder",
+    path = "~/Develop/Apple",
+  },
 }
 
 return {
-  chooserItems = {
-    xcodeLibrary,
-    xcodeDerivedData,
-  },
+  chooserItems = pt.map(function(item)
+    item.subText = item.subText or item.path
+    item.image = item.image or finderIcon
+    item.action = function()
+      local path = pp.expanduser(item.path)
+      os.execute(('open "%s"'):format(path))
+    end
+    return item
+  end, items),
 }
